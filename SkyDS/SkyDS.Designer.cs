f7e6e90400b6766c0851a0b2291f7a3b09d7080f
Alex Vallejo <vallejo.alex@gmail.com>
2014-05-16 15:24:49 -0700

    WIP threading error when trying to update activy log

--- SkyDS/SkyDS.Designer.cs
+++ SkyDS/SkyDS.Designer.cs
@@ -31,26 +31,26 @@
             this.beginWatch = new System.Windows.Forms.Button();
             this.endWatch = new System.Windows.Forms.Button();
             this.choseFilesToSync = new System.Windows.Forms.Button();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.activityLabel = new System.Windows.Forms.Label();
             this.activityArea = new System.Windows.Forms.TextBox();
-            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.remoteMachineName = new System.Windows.Forms.TextBox();
             this.remoteMachineLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // beginWatch
             // 
-            this.beginWatch.Location = new System.Drawing.Point(505, 12);
+            this.beginWatch.Location = new System.Drawing.Point(395, 11);
             this.beginWatch.Name = "beginWatch";
             this.beginWatch.Size = new System.Drawing.Size(104, 24);
             this.beginWatch.TabIndex = 1;
             this.beginWatch.Text = "Sync Files";
             this.beginWatch.UseVisualStyleBackColor = true;
+            this.beginWatch.Click += new System.EventHandler(this.beginWatch_Click);
             // 
             // endWatch
             // 
-            this.endWatch.Location = new System.Drawing.Point(395, 12);
+            this.endWatch.Location = new System.Drawing.Point(505, 11);
             this.endWatch.Name = "endWatch";
             this.endWatch.Size = new System.Drawing.Size(104, 24);
             this.endWatch.TabIndex = 2;
@@ -66,16 +66,20 @@
             this.choseFilesToSync.TabIndex = 3;
             this.choseFilesToSync.Text = "Choose Files to Sync";
             this.choseFilesToSync.UseVisualStyleBackColor = true;
-            this.choseFilesToSync.Click += new System.EventHandler(this.button1_Click);
+            this.choseFilesToSync.Click += new System.EventHandler(this.chooseFilesToSync_Click);
             // 
-            // label1
+            // backgroundWorker
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Activity:";
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            // 
+            // activityLabel
+            // 
+            this.activityLabel.AutoSize = true;
+            this.activityLabel.Location = new System.Drawing.Point(9, 41);
+            this.activityLabel.Name = "activityLabel";
+            this.activityLabel.Size = new System.Drawing.Size(44, 13);
+            this.activityLabel.TabIndex = 4;
+            this.activityLabel.Text = "Activity:";
             // 
             // activityArea
             // 
@@ -89,16 +93,6 @@
             this.activityArea.TabIndex = 5;
             this.activityArea.Text = "Welcome to Sky Directory Sync!\r\n\r\nThis utility will keep your local build directo" +
     "ry in sync with a virtual machine that is hosting skyweb";
-            this.activityArea.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // openFileDialog
-            // 
-            this.openFileDialog.FileName = "openFileDialog";
-            this.openFileDialog.Multiselect = true;
-            this.openFileDialog.ShowHelp = true;
-            this.openFileDialog.ShowReadOnly = true;
-            this.openFileDialog.SupportMultiDottedExtensions = true;
-            this.openFileDialog.Title = "Choose files to sync";
             // 
             // remoteMachineName
             // 
@@ -107,6 +101,7 @@
             this.remoteMachineName.Name = "remoteMachineName";
             this.remoteMachineName.Size = new System.Drawing.Size(120, 20);
             this.remoteMachineName.TabIndex = 6;
+            this.remoteMachineName.TextChanged += new System.EventHandler(this.remoteMachineName_TextChanged);
             // 
             // remoteMachineLabel
             // 
@@ -125,13 +120,13 @@
             this.Controls.Add(this.remoteMachineLabel);
             this.Controls.Add(this.remoteMachineName);
             this.Controls.Add(this.activityArea);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.activityLabel);
             this.Controls.Add(this.choseFilesToSync);
             this.Controls.Add(this.endWatch);
             this.Controls.Add(this.beginWatch);
             this.Name = "SkyDS";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Sky Directory Sync";
+            this.Load += new System.EventHandler(this.SkyDSForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,10 +137,9 @@
         private System.Windows.Forms.Button beginWatch;
         private System.Windows.Forms.Button endWatch;
         private System.Windows.Forms.Button choseFilesToSync;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Label activityLabel;
         private System.Windows.Forms.TextBox activityArea;
-        private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.TextBox remoteMachineName;
         private System.Windows.Forms.Label remoteMachineLabel;
     }
--- SkyDS/SkyDS.cs
+++ SkyDS/SkyDS.cs
@@ -7,34 +7,168 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SkyDS
 {
+    public class ActivityText
+    {
+        public Color Color { get; set; }
+        public string Header { get; set; }
+        public string Msg { get; set; }
+
+        public ActivityText(string header, Color color, string msg)
+        {
+            this.Header = header;
+            this.Color = color;
+            this.Msg = msg;
+        }
+    }
+
     public partial class SkyDS : Form
     {
+        private string filepaths;
+        private Dictionary<string, FileSystemWatcher> watchers = new Dictionary<string,FileSystemWatcher>();
+        private string remoteDirectory;
+
         public SkyDS()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void SkyDSForm_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void chooseFilesToSync_Click(object sender, EventArgs e)
         {
+            var dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+            //dialog.Filter = "All files | *.* | Hidden files | .*";
 
-        }
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string fullpath in dialog.FileNames)
+                {
+                    if (String.IsNullOrEmpty(fullpath))
+                    {
+                        continue;
+                    }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+                    string[] pathComponents = fullpath.Split('\\');
+                    string filename = pathComponents[pathComponents.Length - 1];
+                    string path = fullpath.TrimEnd(filename.ToCharArray());
 
+                    watchers.Add(fullpath, new FileSystemWatcher(path, filename));
+                }
+            }
         }
 
         private void endWatch_Click(object sender, EventArgs e)
         {
+            foreach (FileSystemWatcher watcher in watchers.Values)
+            {
+                watcher.EnableRaisingEvents = false;
+            }
 
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Pass the argument directly back to the RunWorkerCompleted method
+            e.Result = e.Argument;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var activityText = (ActivityText)e.Result;
+            activityArea.ForeColor = activityText.Color;
+            activityArea.AppendText(Environment.NewLine + Environment.NewLine + "--- " + activityText.Header + " ------------------------");
+            activityArea.ForeColor = Color.Black;
+            activityArea.AppendText(Environment.NewLine + activityText.Msg);
+        }
+
+        private void remoteMachineName_TextChanged(object sender, EventArgs e)
+        {
+            var versionNum = "17.500.7707.2003";
+            remoteDirectory = "\\\\" + remoteMachineName.Text + "\\ServicesFE\\SkyWeb\\" + versionNum + "\\web\\bin\\";
+            PrintActivity("Remote machine changed", Color.Green, "Copying to directory: " + remoteDirectory);
+        }
+
+        private void PrintActivity(string heading, Color color, string activity)
+        {
+                var bw = new BackgroundWorker();
+                bw.DoWork += backgroundWorker_DoWork;
+                bw.RunWorkerCompleted += backgroundWorker_RunWorkerCompleted;
+                bw.RunWorkerAsync(new ActivityText(heading, color, activity));
+        }
+
+        private void beginWatch_Click(object sender, EventArgs e)
+        {
+            foreach (FileSystemWatcher watcher in watchers.Values)
+            {
+                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                                       | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                watcher.Changed += OnChanged;
+                watcher.Created += OnChanged;
+                watcher.Deleted += OnDeleted;
+                watcher.Renamed += OnRenamed;
+                watcher.EnableRaisingEvents = true;
+
+                PrintActivity("Now Watching", Color.Green, watcher.Filter);
+            }
+        }
+
+        // Define the event handlers
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted
+            var directoryComponents = e.FullPath.Split('\\');
+            var fileName = directoryComponents[directoryComponents.Length - 1];
+            var remoteFullPath = remoteDirectory + fileName;
+
+            var finished = false;
+            while (!finished)
+            {
+                try
+                {
+                    File.Copy(e.FullPath, remoteFullPath, true);
+                    finished = true;
+                }
+
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+            PrintActivity(e.ChangeType.ToString(), Color.Green, remoteFullPath);
+        }
+
+        private void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            var directoryComponents = e.FullPath.Split('\\');
+            var fileName = directoryComponents[directoryComponents.Length - 1];
+            var remoteFullPath = remoteDirectory + fileName;
+
+            File.Delete(remoteFullPath);
+
+            PrintActivity(e.ChangeType.ToString(), Color.Green, remoteFullPath);
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            var newDirectoryComponents = e.FullPath.Split('\\');
+            var newFileName = newDirectoryComponents[newDirectoryComponents.Length - 1];
+            var oldDirectoryComponents = e.OldFullPath.Split('\\');
+            var oldFileName = oldDirectoryComponents[oldDirectoryComponents.Length - 1];
+
+            var rename = "File: " + e.OldFullPath + "renamed to " + e.FullPath;
+            File.Delete(remoteDirectory + oldFileName);
+            File.Copy(e.FullPath, remoteDirectory + newFileName, true);
+
+            PrintActivity(e.ChangeType.ToString(), Color.Green, rename);
         }
     }
 }
